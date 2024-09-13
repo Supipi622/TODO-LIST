c5c826b6b8b854e5584704bc099d56ca4e61baee
supipi99 <supipi@sutratechnologies.com>
2024-09-13 22:07:25 +0530

    add code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import '../style/pageStyle/Home.css'
 import { useNavigate } from "react-router-dom";
 import TodoForm from "../components/TodoForm";
-import EditTask from "../components/EditTask";
+
 
 const Home = () => {
   const navigate = useNavigate();
